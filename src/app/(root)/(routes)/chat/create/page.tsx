0328582cdf20b66243f7b45531d3a3faca8087cf
babelpainterwell <zhang.zhongw@northeastern.edu>
2023-11-08 21:34:57 -0500

    remove create page.tsx

--- src/app/(root)/(routes)/chat/create/page.tsx
+++
@@ -1,5 +0,0 @@
-// check if in the past the user already has a chat associated with this paper
-// if so, load the chat with the chatId or paperId
-// if not, create a new chat with the paperId and router.push to the chat page. do I need the create chat UI?
-
-// The "start chatting" button should not immediately direct to a specific URL until you have determined whether there is an existing chat or a new one needs to be created. Instead, the button should trigger a function that performs the check and then directs the user to the appropriate URL based on the outcome.
